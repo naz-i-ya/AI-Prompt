8485dfc271b3a318508575ceb7e9020e94f1a8aa
naz-i-ya <naziyanaz1208@gmail.com>
2023-06-17 01:15:16 +0530

    AI-Prompt

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,58 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return(
+    <div className="mt-16 prompt_layout">
+      {
+        data.map((post) => ( 
+          <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+           />
+        ))}
+      
+    </div>
+  )
+}
 
 const Feed = () => {
+  const[searchText, setSearchText] = useState('');
+  const[post, setPost] = useState([]);
+
+  const handleSearchChange = () => {
+
+  }
+
+  useEffect(() => {
+    const fetchPost = async() => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
+      setPost(data);
+    }
+    fetchPost();
+  },[])
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+    <form className="relative w-full flex-center">
+      <input
+      type="text"
+      placeholder="Search for a tag or username"
+      className="search_input"
+      value={searchText}
+      onChange={handleSearchChange}
+      />
+    </form>
+    <PromptCardList 
+    data={post}
+    handleTagClick={() => {}}
+    />
+    <h1>hsdfjdshfj</h1>
+    </section>
   )
 }
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+'use client'
+import { useState } from "react"
+import Image from 'next/image'
+import { useSession } from "next-auth/react"
+import { usePathname, useRouter } from "next/navigation"
 
-const PromptCard = () => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+    <div></div>
+      </div>
+    </div>
   )
 }
 
